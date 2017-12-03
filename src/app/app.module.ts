9e62b6527b97b39ee53f229c7d0dd07d87228a9c
Guilherme Malfatti <guilherme.malfatti@compasso.com.br>
2017-12-03 16:10:40 -0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { MyTeamsPage } from '../pages/my-teams/my-teams.page';
+//import {TournamentsPage} from '../pages/tournaments/tournaments';
+import { TournamentsPage } from '../pages/pages';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MyTeamsPage,
+    TournamentsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MyTeamsPage,
+    TournamentsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
